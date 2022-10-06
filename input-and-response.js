262fbe9d0df86f88df40bf56a649860766b08002
IzaiahLucero <ilucero32@cnm.edu>
2022-10-06 16:57:57 -0600

    Added a description for the calculateTaxes function.

--- input-and-response.js
+++ input-and-response.js
@@ -1,15 +1,16 @@
 /*
- * The below fuction caputers the users input for Tax Rate and Payment Amount
- * after it converts the whole number to a decimal it adds one to that number
- * then it
+ * The below fuction caputers the users input for Tax Rate and Payment Amount.
+ * It then converts the tax rate whole number to a decimal and adds one to that number
+ * then it divides the payment amount by the converted tax rate.
+ * Finally it assigns the result of that division to the variable totalAmount and
+ * add it to the webpage via a new paragraph tag.
  */
-function checkScript() {
 
+function calculateTaxes() {
     let taxRateInput = document.getElementById("taxRate").value;
     let paymentAmountInput = document.getElementById("paymentAmount").value;
     let taxRateConverter = (taxRateInput / 100) + 1;
     let totalAmount = paymentAmountInput / taxRateConverter;
-
 
     const inputRecorder = document.createElement("p");
     inputRecorder.innerHTML = "INCOME = $" + totalAmount.toFixed(2);
