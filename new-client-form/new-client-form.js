9ab893fd8f701cfece45c225d53119329443414b
IzaiahLucero <ilucero32@cnm.edu>
2022-10-23 16:37:26 -0600

    Added a index.html file that will be where I place all the features as I build. Currently added the "New Client" modal.

--- new-client-form/new-client-form.js
+++ new-client-form/new-client-form.js
@@ -2,14 +2,10 @@ function collectNewClientData() {
     let firstNameInput = document.getElementById("firstName").value;
     let lastNameInput = document.getElementById("lastName").value;
     let primaryPhoneInput = document.getElementById("primaryPhone").value;
-    let secondaryPhoneInput = document.getElementById("secondaryPhoneNumber").value;
+    let secondaryPhoneInput = document.getElementById("secondaryPhone").value;
     let emailAddressInput = document.getElementById("emailAddress").value;
     let projectStreetAddressInput = document.getElementById("projectStreetAddress").value;
-    let projectCityInput = document.getElementById("projectCity").value;
-    let projectStateInput = document.getElementById("projectState").value;
-    let projectZipInput = document.getElementById("projectZip").value;
-    let locationCodeInput = document.getElementById("locationCode").value;
-    console.log(firstNameInput + ", " + lastNameInput + ", " + primaryPhoneInput + ", " + secondaryPhoneInput + ", " + emailAddressInput + ", " + projectStreetAddressInput + ", " + projectCityInput + ", " + projectStateInput + ", " + projectZipInput);
+    return console.log(firstNameInput + ", " + lastNameInput + ", " + primaryPhoneInput + ", " + secondaryPhoneInput + ", " + emailAddressInput + ", " + projectStreetAddressInput);
 
 }
 
